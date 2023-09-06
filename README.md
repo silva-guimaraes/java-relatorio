a68735f47468de044e3ece4dd03e124a4eff491a
silva guimaraes <silva__guimaraes@hotmail.com>
2023-09-06 11:25:28 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-projeto-relatorio
+
+Projeto em java para as aulas de 'Programação Orientada a Objeto' pelo professor Heráclito Amancio Pereira Junior para a Universidade Vila Velha.
---
+++ relatorio.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Scanner;
+
+
+class relatorio {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        var foo = new Scanner(System.in);
+        System.out.println(foo.next());
+    }
+}
